6e18796c81529e5f23ec91517368795e62330e47
ikatama <ikatama@example.com>
2023-02-19 14:30:41 +0000

    [linux]brewのインストールにdockerとgitを追加

--- init.sh
+++ init.sh
@@ -9,4 +9,12 @@ else
     brew install fd
     brew install exa
     brew install bat
+    brew install docker
+    brew install git
 fi
+
+DOTFILE = "$HOME/dotfiles"
+ln -sf DOTFILE/vim/.vimrc ~/.vimrc
+ln -sf DOTFILE/bash/.bashrc ~/.bashrc
+ln -sf DOTFILE/bash/.bash_profile ~/.bash_profile
+
